0569cacd3bb1aee517d4e447a1e19e3ee32de14d
samuel schwob <samuelschwob@gmail.com>
2021-03-08 17:36:46 +0100

    First setup project

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, format: { with: /\A(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}\z/, message: "Invalid phone number" }
 end
