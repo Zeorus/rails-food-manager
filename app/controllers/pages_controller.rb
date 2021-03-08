edfb9f6e632c9e6680b390a0003814820ca3e0be
samuel schwob <samuelschwob@gmail.com>
2021-03-08 21:03:22 +0100

    Model, Controller & Seed customer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    render :dashboard
+  end
+
+  def dashboard
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Clean DB customers"
+Customer.destroy_all
+
+puts "Generate Customers"
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0654789645", 
+                address: "57 quai Saint-Nicolas", city: "Tourcoing", zip_code: "59200")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0745421895", 
+                address: "35 rue de l'Epeule", city: "Roubaix", zip_code: "59100")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0641287964", 
+                address: "4 Chemin Challet", city: "Lille", zip_code: "59000")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0965847852", 
+                address: "91 Rue du Général Sarrail", city: "Roubaix", zip_code: "59100")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0745876312", 
+                address: "122 rue Solférino", city: "Lille", zip_code: "59000")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0648571236", 
+                address: "15 rue du Faison", city: "Lille", zip_code: "59000")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0625789645", 
+                address: "1 place Notre Dame", city: "Tourcoing", zip_code: "59200")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0315784952", 
+                address: "101 boulevard Descat", city: "Tourcoing", zip_code: "59200")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0625489675", 
+                address: "228 rue Carnot", city: "Wattrelos", zip_code: "59150")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0948657152", 
+                address: "18 rue Jean Jaurès", city: "Wattrelos", zip_code: "59150")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0745312589", 
+                address: "12 rue du Maréchal Foch", city: "Roubaix", zip_code: "59100")
+Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: "0641251489", 
+                address: "12 rue Alain Savary", city: "Wattrelos", zip_code: "59150")
+
+puts "#{Customer.count} customers generated!"
