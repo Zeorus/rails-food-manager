d7760f70f025c3d7bed09df802ca3fc895871115
samuel schwob <samuelschwob@gmail.com>
2021-03-08 22:16:16 +0100

    Setup Order_Product Model & Controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :photo
+
+  has_many :order_products
   
   validates :name, :category, presence: true
   validates :price, :price_in_menu, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_204506) do
+ActiveRecord::Schema.define(version: 2021_03_08_205901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,17 @@ ActiveRecord::Schema.define(version: 2021_03_08_204506) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "order_products", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "order_id", null: false
+    t.integer "quantity", default: 1
+    t.boolean "belongs_menu", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.bigint "customer_id", null: false
     t.string "payement"
@@ -94,5 +105,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_204506) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
   add_foreign_key "orders", "customers"
 end
