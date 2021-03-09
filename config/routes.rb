d798b9b945703f5ecd20ac6f66d7232d72e3ab68
samuel schwob <samuelschwob@gmail.com>
2021-03-09 03:43:48 +0100

    First design navbar & sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'dashboard', to: 'pages#dashboard'
+
+  resources :customers, only: [:index]
+
+  resources :orders, only: [:index]
+
+  resources :products, only: [:index]
 end
