953597638f73ee8d3cc1dfe14e06b1a98b2d9fb1
kevin sayarath <kevin.sayarath@hotmail.fr>
2021-03-09 15:31:25 +0100

    CRUD customer ready

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,54 @@
 class CustomersController < ApplicationController
+  before_action :find_customer, only: [:edit, :update, :destroy]
 
   def index
     @customers = policy_scope(Customer)
   end
+
+  def new
+    @customer = Customer.new
+    authorize @customer
+  end
+
+
+  def create
+    @customer = Customer.new(customer_params)
+    authorize @customer
+    if @customer.save
+      redirect_to @customers, notice: "Customer was successfully created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    authorize @customer
+  end
+
+  def update
+    authorize @customer
+    if @customer.update(customer_params)
+      redirect_to customers_path, notice: "Customer was successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize @customer
+    @customer.destroy
+    head :no_content
+  end
+
+
+  private
+
+  def find_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :phone_number, :address, :zip_code, :city, :email)
+  end
+
 end
--- app/policies/customer_policy.rb
+++ app/policies/customer_policy.rb
@@ -4,4 +4,26 @@ class CustomerPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def new?
+    @user.role == "admin"
+  end
+
+  def create?
+    @user.role == "admin"
+  end
+
+  def edit?
+    @user.role == "admin"
+  end
+
+  def update?
+    @user.role == "admin"
+  end
+
+  def destroy?
+    @user.role == "admin"
+  end
+
+
 end
