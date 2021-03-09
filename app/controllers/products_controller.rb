2da2fedd121ac53fb88141dee31f219ccecb7239
maxime ayllon <maximeayllon12@gmail.com>
2021-03-09 12:18:46 +0100

    product controller, policy and cloudinary key

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,52 @@ class ProductsController < ApplicationController
   def index
     @products = policy_scope(Product)
   end
+
+  def show
+    @product = Product.find(params[:id])
+    authorize @product
+  end
+
+  def new
+    @product = Product.new
+    authorize @product
+  end
+
+  def create
+    @product = Product.new
+    authorize @product
+    if @product.save(product_params)
+      render :index
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    authorize @product
+  end
+
+  def updates
+    @product = Product.new
+    authorize @product
+    if @product.update(product_params)
+      render :index
+    else
+      render :show
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    authorize @product
+    @product.destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:restaurant).permit(:name, :price, :price_in_menu,
+                                       :description, :category, :sub_category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:index]
 
-  resources :products, only: [:index]
+  resources :products, except: [:show]
 end
